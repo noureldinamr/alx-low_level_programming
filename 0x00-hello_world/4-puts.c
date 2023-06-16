da004ad20a881e64e94a2a44c0f7e14d457c8e5a
Noureldin amr <noureldinamr94@gmail.com>
2023-06-16 06:19:31 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-/* main - C program that prints a line using puts function*/
+      #include<stdio.h>
+      /* main - C program that prints a line using puts function*/
 
-/* Return: 0 (the code end with zero is correct) */
-int main (void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+      /* Return: 0 (the code end with zero is correct) */
+      int main (void)
+      {
+          puts("\"Programming is like building a multilingual puzzle");
+          return (0);
+      }
