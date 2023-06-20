f7af7b0656c289b8c9edd7e5ebccd0eb271b1028
Noureldin amr <noureldinamr94@gmail.com>
2023-06-20 12:11:55 -0700

    aa

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_alphabet - 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; ch <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
