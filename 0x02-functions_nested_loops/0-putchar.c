501bfc172dd0154234275dbfdf26e9502bac50e6
Noureldin amr <noureldinamr94@gmail.com>
2023-06-20 10:30:00 -0700

    aa

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-_Putchar('_');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -14,7 +14,7 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n');
+_putchar('\n')i;
 return (0);
 }
 
