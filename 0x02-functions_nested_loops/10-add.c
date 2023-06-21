cdf7f708c3c7d87d900be3da754829006a109051
Noureldin amr <noureldinamr94@gmail.com>
2023-06-21 02:54:10 -0700

    aa

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first no. to compute
+ * @n2: The second no. to compute
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
