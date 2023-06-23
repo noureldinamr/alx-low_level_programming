e34cb7f683ca238eb86c390d7b360e9d3ce83999
Noureldin amr <noureldinamr94@gmail.com>
2023-06-23 11:01:45 -0700

    aa

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar write a character c to stdout
+ * @c: The charcter to print
+ * Return: 1 (success)
+ * On error, -1 is returned, and error is se approprapetly 
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, %c, 1));
+}
