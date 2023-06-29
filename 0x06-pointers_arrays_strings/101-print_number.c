4dfb0bc2ba2a4337b3901e27edd6fb741ad27fbb
Noureldin amr <noureldinamr94@gmail.com>
2023-06-29 05:01:06 -0700

    aa

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio>
 
 /**
   * print_number - Prints any integer with putchar
@@ -12,7 +13,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n *= -1;
 	}
 
@@ -21,5 +22,5 @@ void print_number(int n)
 	if (x / 10)
 		print_number(x / 10);
 
-	_putchar(x % 10 + '0');
+	putchar(x % 10 + '0');
 }
