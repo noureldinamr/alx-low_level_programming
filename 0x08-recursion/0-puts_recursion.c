5084db75582620142ef1fcbfd7d3e09416491b47
Noureldin amr <noureldinamr94@gmail.com>
2023-07-05 07:44:49 -0700

    aa

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -11,6 +12,6 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	putchar(*s);
+	puts_recursion(s + 1);
 }
