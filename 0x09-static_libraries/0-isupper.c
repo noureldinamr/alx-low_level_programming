af80cd7584dbd0c19765be8704946ac97a0d6ad4
Noureldin amr <noureldinamr94@gmail.com>
2023-07-07 15:23:44 -0700

    aa

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for the uper case character
+ * @c: the character to be checked 
+ * Return: 1 (success)
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
